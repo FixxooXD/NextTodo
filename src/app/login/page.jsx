95d01c89cc07ef8e13575a1483878a901791eb5d
Fixxoo <rajpalmal40@gmail.com>
2024-03-04 23:25:53 +0530

    Working on Bugs

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -53,7 +53,7 @@ const SignUpForm = () => {
               </div>
               <button onClick={handleSubmit} type="submit" className="w-full text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Dont have an account? <a href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Getsignup</a>
+                Don&apos;t have an account? <a onClick={()=> router.push('/signup')} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Getsignup</a>
               </p>
             </form>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation"
 
-export default function home(){
+export default function Home(){
   const router = useRouter();
   return(
     <div className="landing-page">
@@ -12,11 +12,11 @@ export default function home(){
           <p className="text-xl text-white mt-4">
             Track your progress, stay motivated, and achieve your daily goals with Next Todo.
           </p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded shadow-md mt-8" onClick={()=> router.push("/signup")}>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded shadow-md mt-8" onClick={() => router.push("/signup")}>
             Get Started
           </button>
         </div>
       </section>
-      </div>
+      </div>  
   )
 }
