cf59c15b312aa8959d417dad7510fe59081fe6be
Fixxoo <rajpalmal40@gmail.com>
2024-03-06 20:51:23 +0530

    Worked in Password & confirmPassword field

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -8,16 +8,23 @@ import Footer from "@/components/Footer";
 const SignUpForm = () => {
   const router = useRouter();
   const [processing, setProcessing] = React.useState(false);
+  //Todo: WE Can set specific message on on specific fields 
+  const [focus, setUserFocus] = React.useState(false);
   const [user, setUser] = React.useState({
     userName: "",
     email: "",
-    password: ""
+    password: "",
+    conFirmPassword: ""
   })
 
 
   const handleSubmit = async (e) => {
      e.preventDefault()
      setProcessing(true);
+     if (user.password !== user.conFirmPassword) {
+      setProcessing(false);
+        return console.log("Dont Proceed");
+     }
      try {
       const response =  await axios.post("/api/users/signup", user)
       console.log(response);
@@ -56,7 +63,9 @@ const SignUpForm = () => {
               </div>
               <div>
                 <label htmlFor="confirm-password" className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                <input type="password" name="confirm-password" id="confirm-password" onChange={(e) => setUser({...user, password: e.target.value})} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                <input type="password" name="confirm-password" id="confirm-password" onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}  onChange={(e) => setUser({...user, conFirmPassword: e.target.value})} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                   <div>{user.password !== user.conFirmPassword && focus ? <span>Must Match the Password and Field</span>: null}</div>
               </div>
               <button disabled={!user.email || !user.password ? true : false } onClick={handleSubmit} type="submit" className="w-full text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
